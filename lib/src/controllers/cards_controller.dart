214dd930acb97bc102eb2aee4d649546b58ac1d4
Randy Buhendwa <creytas@gmail.com>
2022-11-02 16:09:26 +0100

    feat: create a card controller

---
+++ lib/src/controllers/cards_controller.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:shoppy_app/src/models/card_model.dart';
+
+final firstCard = CardModel(
+    'nouilles-sautees-express.jpg',
+    'Nouilles sautées EXPRESS à la chinoise',
+    'Plat de nouilles sautées, réalisé par la chef Ling Xiaoyu',
+    25.00);
